bf63dd40fc4ff9b7fe26c07aed5bc763e8ddffca
sariT11111 <tesfayesaron65@gmail.com>
2026-05-25 13:23:07 -0700

    Null safety

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("=== Null Safety Demo ===");
+
+string? region = null;
+
+Console.WriteLine(region);
+
+string displayRegion = region ?? "No Region Assigned";
+
+Console.WriteLine(displayRegion);
+
+region ??= "Addis Ababa";
+
+Console.WriteLine(region);
+
+Console.WriteLine(region?.ToUpper());
